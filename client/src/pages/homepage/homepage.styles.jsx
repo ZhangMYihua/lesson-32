4dce47547a582d8c5c2e1a1e5c4ef9c680de6789
SilverM <SilverM@users.noreply.github.com>
2020-10-13 18:45:35 +0800

    Main page sizing off

--- client/src/pages/homepage/homepage.styles.jsx
+++ client/src/pages/homepage/homepage.styles.jsx
@@ -65,6 +65,6 @@ export const ContentContainer = styled.div`
 export const ContentTitle = styled.span`
   // font-weight: bold;
   margin-bottom: 6px;
-  font-size: 40px;
+  font-size: min(max(35px, 3vw), 40px);
   color: white;
 `;
